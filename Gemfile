1c32d9731c0055b8ce3abff6d3a013df37b2857b
blackdidi <blackdidi@gmail.com>
2013-06-27 20:16:02 +0800

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -6,10 +6,21 @@ gem 'rails', '3.2.3'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 
-group :development, :test do
+group :development do
     gem 'sqlite3', '1.3.5'
     gem 'rspec-rails', '2.9.0'
     gem 'capybara', '1.1.2'
+    gem 'guard-rspec', '0.5.5'
+end
+
+group :test do
+gem 'guard'
+gem 'sqlite3', '1.3.5'
+gem 'rspec-rails', '2.9.0'
+gem 'capybara', '1.1.2'
+# System-dependent gems
+gem 'rb-fsevent', '>=0.4.3.1', :require => false
+gem 'growl', '1.0.3'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,14 +6,14 @@ describe "Static pages" do
     let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
     describe "Home page" do
-        it "should have the content 'Sample App'" do
+        it "should have the h1 'Sample App'" do
             visit '/static_pages/home'
-            page.should have_content('Sample App')
+            page.should have_selector('h1', :text => 'Sample App')
         end
         it "should have the title 'Home'" do
             visit '/static_pages/home'
             page.should have_selector('title',
-            :text => "Ruby on Rails Tutorial Sample App | Home")
+            :text => "Ruby on Rails Tutorial Sample App")
         end
     end
     describe "Help page" do
